4cf03d52a195a9d6ad84e0dd33e6b7f988380cd2
Aswin Vijay <51475229+mastergreyhat@users.noreply.github.com>
2025-04-29 11:08:58 +0000

    add bmi calculator code

---
+++ day_2/bmi_calculator.py
@@ -0,0 +1,26 @@
+#BMI calculator
+weight = float(input("Enter your weight (in Kilograms): "))
+height = float(input("Enter your height (in metres): "))
+bmi = weight/height**2
+
+# print(type(bmi)) -> gives type as float
+
+''' follows PEMDAS -> (Paranthesis, exponent, multiplication, division, addition, subtraction)
+    with division and multiplication having equal priority, hence evaluated from left to right '''
+
+print(f"Your BMI: {round(bmi, 2)}")
+
+''' here 
+        round(value)          : strips off the decimal values, and 
+        round(value, number)  : keeps the number of decimal places we pass '''
+
+if bmi < 18.5:
+    print("You are underweight")
+elif bmi >= 18.5 and bmi < 24.9:
+    print("You are normal weight")
+elif bmi >= 24.9 and bmi < 29.9:
+    print("You are overweight")
+elif bmi >= 29.9 and bmi < 34.9:
+    print("You are obese")
+elif bmi >= 34.9:
+    print("You are extremely obese")
