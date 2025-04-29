e88eaa9a75f426d0a86921f5d6a4cc32ae4bb157
Aswin Vijay <51475229+mastergreyhat@users.noreply.github.com>
2025-04-29 07:15:27 +0000

    band name generator code

---
+++ day_1/band_name_generator.py
@@ -0,0 +1,4 @@
+city_name = input("Enter the name of your city: ")
+animal_name = input("Enter name of an animal: ")
+# print("Name of your band: " + city_name + " " + pet_name)
+print(f"Name of your band could be: {city_name} {animal_name}s")
