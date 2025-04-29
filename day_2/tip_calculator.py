6bd227800dc76a28c24633e72d9f46187568da84
Aswin Vijay <51475229+mastergreyhat@users.noreply.github.com>
2025-04-29 11:39:34 +0000

    add tip calculator code

--- day_2/tip_calculator.py
+++ day_2/tip_calculator.py
@@ -3,9 +3,9 @@ total_bill = float(input("Enter the total bill amount: $"))
 tip = int(input("what percentage of tip would you like to give? 10, 12 or 15? "))
 people = int(input("How many people to split the bill? "))
 
-''' tip_amount = total_bill * tip / 100
+''' tip_amount     = total_bill * tip / 100
     total_with_tip = total_bill + tip_amount
-    per_head = total_with_tip / people '''
+    per_head       = total_with_tip / people '''
 
 #combining above 3 steps into a single line
 per_head = (total_bill * (tip/100) + total_bill)/people
