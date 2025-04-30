1e7830890a3e6b1cdd3477b4ff6a44809c93606f
Aswin Vijay <51475229+mastergreyhat@users.noreply.github.com>
2025-04-30 20:43:16 +0000

    better implementation

--- day_3/python_pizza.py
+++ day_3/python_pizza.py
@@ -1,31 +1,26 @@
 print("Welcome to Python Pizza Deliveries!\n")
 size = input("What size do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese: Y or N: ")
 
 if size == "S":
     bill = 15
-    if pepperoni == "Y":
-        bill += 2
-    if extra_cheese == "Y":
-        bill += 1
-    print(f"Your bill amount is: ${bill}")
-
 elif size == "M":
     bill = 20
-    if pepperoni == "Y":
-        bill += 3
-    if extra_cheese == "Y":
-        bill += 1
-    print(f"Your bill amount is: ${bill}")
-
 elif size == "L":
     bill = 25
-    if pepperoni == "Y":
-        bill += 3
-    if extra_cheese == "Y":
-        bill += 1
-    print(f"Your bill amount is: ${bill}")
-
 else:
     print("Invalid option")
+    exit()
+
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese: Y or N: ")
+
+if pepperoni == 'Y':
+    if size == 'S':
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == 'Y':
+    bill += 1
+
+print(f"Your bill amount is: ${bill}")
