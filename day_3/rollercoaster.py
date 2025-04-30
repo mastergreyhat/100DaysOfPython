2f8ecafd3ba91e4e117b0039127d450ab91cee2a
Aswin Vijay <51475229+mastergreyhat@users.noreply.github.com>
2025-04-30 20:07:03 +0000

    remove unwanted else block for bill amount

--- day_3/rollercoaster.py
+++ day_3/rollercoaster.py
@@ -1,8 +1,10 @@
 height = int(input("Enter your height in cm: "))
 bill = 0
+
 if height >=120:
     print("You can ride the rollercoaster !")
     age = int(input("Enter your age: "))
+    
     if age <= 12:
         print("Child tickets are $5")
         bill = 5
@@ -14,10 +16,11 @@ if height >=120:
         bill = 12
     
     want_photo = input("Do you want to take a photo with an added cost of $3 ? type y for yes and n for no: ")
+    
     if want_photo == 'y':
         bill += 3  #can also be written as 'bill = bill + 3'
-        print(f"Your bill amount is: ${bill}")
-    else:  #here any input other than 'y' will execute below else statement
-        print(f"Your bill amount is: ${bill}")
+    
+    print(f"Your bill amount is: ${bill}")
+
 else:
     print("You cannot ride the rollercoaster")
