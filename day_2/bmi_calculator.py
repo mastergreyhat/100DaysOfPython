9c1eb8f7064d1dc16309c95e09e5e3b742b3b31b
Aswin Vijay <51475229+mastergreyhat@users.noreply.github.com>
2025-04-30 19:31:13 +0000

    modify day_2 bmi code to be clean and concise

--- day_2/bmi_calculator.py
+++ day_2/bmi_calculator.py
@@ -14,13 +14,25 @@ print(f"Your BMI: {round(bmi, 2)}")
         round(value)          : strips off the decimal values, and 
         round(value, number)  : keeps the number of decimal places we pass '''
 
+# if bmi < 18.5:
+#     print("You are underweight")
+# elif bmi >= 18.5 and bmi < 24.9:
+#     print("You are normal weight")
+# elif bmi >= 24.9 and bmi < 29.9:
+#     print("You are overweight")
+# elif bmi >= 29.9 and bmi < 34.9:
+#     print("You are obese")
+# elif bmi >= 34.9:
+#     print("You are extremely obese")
+
+#modified the above code in an efficient and concise way
 if bmi < 18.5:
     print("You are underweight")
-elif bmi >= 18.5 and bmi < 24.9:
+elif bmi < 24.9:
     print("You are normal weight")
-elif bmi >= 24.9 and bmi < 29.9:
+elif bmi < 29.9:
     print("You are overweight")
-elif bmi >= 29.9 and bmi < 34.9:
+elif bmi < 34.9:
     print("You are obese")
-elif bmi >= 34.9:
+else:
     print("You are extremely obese")
